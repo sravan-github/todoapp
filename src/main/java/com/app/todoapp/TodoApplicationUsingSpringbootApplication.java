8be0f88306efd4188921d10751045bd9cd3dbfd2
sravan-github <39382490+sravan-github@users.noreply.github.com>
2025-01-30 13:01:03 +0530

    Add files via upload

---
+++ src/main/java/com/app/todoapp/TodoApplicationUsingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.app.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplicationUsingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplicationUsingSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/todoapp/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.app.todoapp.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.app.todoapp.models.Task;
+
+public interface TaskRepository extends JpaRepository<Task,Long>{
+
+}
